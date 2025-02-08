050300824218c277928d2e51fab0df471445bd9e
Artyom-exe <thlambot@hotmail.fr>
2025-02-08 15:49:16 +0100

    Gestion de l'image dans le ModuleController : ajout de la prise en charge des fichiers d'image et définition d'une image par défaut dans le modèle Module.

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -10,29 +10,23 @@ class ModuleSeeder extends Seeder
 {
     public function run()
     {
-        $defaultImageUrl = 'https://placehold.co/600x400/e2e8f0/475569?text=Module';
 
         $modules = [
             [
                 'name' => 'Mathematics',
-                'description' => 'Advanced Math Course',
                 'professor_id' => 1,
                 'year_id' => 1,
-                'image_path' => $defaultImageUrl
+
             ],
             [
                 'name' => 'Physics',
-                'description' => 'Intro to Physics',
                 'professor_id' => 2,
                 'year_id' => 1,
-                'image_path' => $defaultImageUrl
             ],
             [
                 'name' => 'Computer Science',
-                'description' => 'Programming Fundamentals',
                 'professor_id' => 1,
                 'year_id' => 2,
-                'image_path' => $defaultImageUrl
             ]
         ];
 
