6edc141e476dedefdaacfcdab130819dd969f7f1
Artyom-exe <th.lambot@hotmail.fr>
2025-02-08 16:06:53 +0100

    Mise à jour des seeders : ajout de la création dynamique d'années, d'étudiants, d'inscriptions, de professeurs, de formulaires, de questions de formulaire et de réponses avec des données aléatoires.

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -4,30 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Module;
-use Illuminate\Support\Facades\Storage;
 
 class ModuleSeeder extends Seeder
 {
     public function run()
     {
-
         $modules = [
-            [
-                'name' => 'Mathematics',
-                'professor_id' => 1,
-                'year_id' => 1,
-
-            ],
-            [
-                'name' => 'Physics',
-                'professor_id' => 2,
-                'year_id' => 1,
-            ],
-            [
-                'name' => 'Computer Science',
-                'professor_id' => 1,
-                'year_id' => 2,
-            ]
+            ['name' => 'Mathématiques Avancées', 'professor_id' => 1, 'year_id' => 1],
+            ['name' => 'Physique Quantique', 'professor_id' => 2, 'year_id' => 1],
+            ['name' => 'Programmation Web', 'professor_id' => 3, 'year_id' => 1],
+            ['name' => 'Base de données', 'professor_id' => 3, 'year_id' => 2],
+            ['name' => 'Intelligence Artificielle', 'professor_id' => 4, 'year_id' => 2],
+            ['name' => 'Cybersécurité', 'professor_id' => 5, 'year_id' => 2],
+            ['name' => 'Réseaux', 'professor_id' => 4, 'year_id' => 3],
+            ['name' => 'DevOps', 'professor_id' => 5, 'year_id' => 3],
         ];
 
         foreach ($modules as $module) {
